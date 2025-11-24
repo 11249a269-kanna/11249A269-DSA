5a217b2567a83585e548b6fbc8ddf42f9e432d11
11249a269-kanna <11249a269@kanchiuniv.ac.in>
2025-11-24 13:32:26 +0530

    Update EXP NO 9.c

--- EXP NO 9.c
+++ EXP NO 9.c
@@ -1,70 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct Node {
     int data;
-    struct Node* left;
-    struct Node* right;
+    struct Node *left;
+    struct Node *right;
 };
 
-
-struct Node* createNode(int data) {
+struct Node* createNode(int value) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->left = newNode->right = NULL;
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
     return newNode;
 }
 
-
 void inorder(struct Node* root) {
-    if (root != NULL) {
-        inorder(root->left);
-        printf("%d ", root->data);
-        inorder(root->right);
-    }
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
 }
-
 
 void preorder(struct Node* root) {
-    if (root != NULL) {
-        printf("%d ", root->data);
-        preorder(root->left);
-        preorder(root->right);
-    }
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
 }
-
 
 void postorder(struct Node* root) {
-    if (root != NULL) {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->data);
-    }
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
 }
 
-
 int main() {
-
     struct Node* root = createNode(1);
     root->left = createNode(2);
     root->right = createNode(3);
     root->left->left = createNode(4);
     root->left->right = createNode(5);
-    root->right->left = createNode(6);
-    root->right->right = createNode(7);
 
-    printf("Inorder Traversal: ");
+    printf("Inorder Traversal   : ");
     inorder(root);
-    printf("\n");
 
-    printf("Preorder Traversal: ");
+    printf("\nPreorder Traversal  : ");
     preorder(root);
-    printf("\n");
 
-    printf("Postorder Traversal: ");
+    printf("\nPostorder Traversal : ");
     postorder(root);
-    printf("\n");
 
     return 0;
 }
